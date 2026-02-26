5730304e7a580ef0a2c71593c18ee611469ebcf7
Elima Mederbek <emederbekovna11@gmail.com>
2026-02-26 10:51:13 -0800

    added info about history

--- Hello.java
+++ Hello.java
@@ -6,5 +6,11 @@ public void HellowJava(){
 public void HellowJava(){
         System.out.println("Hello Java");
 
+
 }
+
+
+        //THIS is error
+}
+// CORRECTEDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 
